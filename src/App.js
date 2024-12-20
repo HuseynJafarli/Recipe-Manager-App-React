18d2746804529f3e61cd6b84f4d57997d41fbda3
Huseyn Jafarli <hjeferli18088@ada.edu.az>
2024-12-20 23:47:11 +0400

    Card style changed, Info page added (Might change the structure later)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
+import RecipeInfoPage from './pages/RecipeInfoPage';
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/recipe/:id" element={<RecipeInfoPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/RecipeInfoPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import RecipeInfo from '../components/RecipeInfo/RecipeInfo'
+function RecipeInfoPage() {
+  return (
+    <RecipeInfo />
+  )
+}
+
+export default RecipeInfoPage
